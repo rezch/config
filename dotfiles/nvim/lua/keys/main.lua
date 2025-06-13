94eea7e944bc85230484564366600dc2db13b16a
rezch <a@b>
2025-06-13 16:24:47 +0300

    nvim add buffer moves

--- dotfiles/nvim/lua/keys/main.lua
+++ dotfiles/nvim/lua/keys/main.lua
@@ -5,18 +5,17 @@ im('<C-k>', '<escape>')
 
 
 -- [[ Tabs ]] --
-nm('<C-S-Right>', ':<C-U>tabprevious<CR>')
-im('<C-S-Right>', '<C-\\><C-N>:tabprevious<CR>')
-vm('<C-S-Right>', '<C-C>:tabprevious<CR>')
-nm('<C-S-Left>', ':<C-U>tabnext<CR>')
-im('<C-S-Left>', '<C-\\><C-N>:tabnext<CR>')
-vm('<C-S-Left>', '<C-C>:tabnext<CR>')
+nm('<C-Right>', '<cmd>bnext<CR>')
+vm('<C-Right>', '<C-C>:bnext<CR>')
+im('<C-Right>', '<C-O>:bnext<CR>')
+nm('<C-Left>', '<cmd>bprevious<CR>')
+vm('<C-Left>', '<C-C>:bprevious<CR>')
+im('<C-Left>', '<C-O>:bprevious<CR>')
 
-nm('<leader>q', ':<C-u>tabclose<CR>')
-im('<leader>q', '<C-\\><C-n>:tabclose<CR>')
-vm('<leader>q', '<C-c>:tabclose<CR>')
+nm('<leader>q', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>')
+vm('<leader>q', '<C-C>:bp<bar>sp<bar>bn<bar>bd<CR>')
+im('<leader>q', '<C-O>:bp<bar>sp<bar>bn<bar>bd<CR>')
 
 
 -- [[ Search highlight ]] --
 nm('<leader>s', ':noh<CR>')
-

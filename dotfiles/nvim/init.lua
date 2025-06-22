41d9b7b57ecfd6e946b5c48946ba02d2a2ee7035
rezch <a@b>
2025-06-22 05:24:36 +0300

    upd

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -13,8 +13,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.cmd([[
-    autocmd filetype cpp inoremap <F5> <Esc> :w <bar> exec '!g++ -std=c++20 -DUwU '.shellescape('%').' -o '.shellescape('%:r').' && ./'.shellescape('%:r').' && rm '.shellescape('%:r')<CR>
-    autocmd filetype cpp nnoremap <F5> <Esc> :w <bar> exec '!g++ -std=c++20 -DUwU '.shellescape('%').' -o '.shellescape('%:r').' && ./'.shellescape('%:r').' && rm '.shellescape('%:r')<CR>
+    autocmd filetype cpp inoremap <F5> <Esc> :w <bar> exec '!g++ -std=c++2b -DUwU '.shellescape('%').' -o '.shellescape('%:r').' && ./'.shellescape('%:r').' && rm '.shellescape('%:r')<CR>
+    autocmd filetype cpp nnoremap <F5> <Esc> :w <bar> exec '!g++ -std=c++2b -DUwU '.shellescape('%').' -o '.shellescape('%:r').' && ./'.shellescape('%:r').' && rm '.shellescape('%:r')<CR>
 
     autocmd filetype sh inoremap <F5> <Esc> :w <bar> exec '!bash '.shellescape('%')<CR>
     autocmd filetype sh nnoremap <F5> <Esc> :w <bar> exec '!bash '.shellescape('%')<CR>

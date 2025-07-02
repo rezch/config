94990bd2c719bd55e9e9df1e52691266fd6838a3
rezch <a@b>
2025-07-03 01:33:05 +0300

    nvim ref

--- dotfiles/nvim/lua/keys/main.lua
+++ dotfiles/nvim/lua/keys/main.lua
@@ -11,9 +11,10 @@ map.nm('<CA-Left>', '<cmd>bprevious<CR>')
 map.vm('<CA-Left>', '<C-C>:bprevious<CR>')
 map.im('<CA-Left>', '<C-O>:bprevious<CR>')
 
-map.nm('<leader>q', '<cmd>bp<bar>sp<bar>bn<bar>bd!<CR>')
-map.vm('<leader>q', '<C-C>:bp<bar>sp<bar>bn<bar>bd!<CR>')
-map.im('<leader>q', '<C-O>:bp<bar>sp<bar>bn<bar>bd!<CR>')
+local close_one_buf = ':bp<bar>sp<bar>bn<bar>bd!<CR>'
+map.nm('<leader>q', close_one_buf)
+map.vm('<leader>q', '<C-C>' .. close_one_buf)
+map.im('<leader>q', '<C-O>' .. close_one_buf)
 
 -- [[ Search highlight ]] --
 map.nm('<leader>s', ':noh<CR>')
@@ -23,3 +24,4 @@ map.all('<C-S>', '<cmd>update<CR>')
 
 -- [[ Terminal ]] --
 vim.api.nvim_set_keymap('t', '<Leader><ESC>', '<C-\\><C-n>', { noremap = true })
+vim.api.nvim_set_keymap('t', '<Leader>q', '<C-\\><C-n>' .. close_one_buf, { noremap = true })
